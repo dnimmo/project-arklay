8c60d564e0fef732e6c0269856d58d6db5610ae0
David Nimmo <dnimmo@gmail.com>
2015-07-23 13:04:29 +0000

    Changed inventory styles, added inventory count to collapsed view

--- src/assets/scripts/app.js
+++ src/assets/scripts/app.js
@@ -301,3 +301,24 @@ app.controller('MainCtrl', ['$scope', 'GameMapFactory', 'GameItemFactory', 'Cred
     $scope.$apply();
   });
 }]);
+
+// Fullscreen test
+var pfx = ["webkit", "moz", "ms", "o", ""];
+function RunPrefixMethod(obj, method) {
+	
+	var p = 0, m, t;
+	while (p < pfx.length && !obj[m]) {
+		m = method;
+		if (pfx[p] == "") {
+			m = m.substr(0,1).toLowerCase() + m.substr(1);
+		}
+		m = pfx[p] + m;
+		t = typeof obj[m];
+		if (t != "undefined") {
+			pfx = [pfx[p]];
+			return (t == "function" ? obj[m]() : obj[m]);
+		}
+		p++;
+	}
+
+}
