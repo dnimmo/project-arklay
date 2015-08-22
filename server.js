ca104c2b9d0d050d1fce444bd6aaf4140eb5090c
David Nimmo <dnimmo@gmail.com>
2015-08-22 08:58:52 +0000

    added GZIP compression

--- server.js
+++ server.js
@@ -6,12 +6,16 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
+var compression = require('compression');
 // Serve on port 8080 in dev but port 80 in live! Make this programatic at some point because it's annoying now. :P
 var port = 80;
 
 // Load the map and credits
 var data = JSON.parse(fs.readFileSync('src/assets/resources/map.json', 'utf8'));
 var credits = JSON.parse(fs.readFileSync('src/assets/resources/credits.json', 'utf8'));
+
+//  GZIP assets
+app.use(compression());
 
 // Serve files
 app.use(express.static(__dirname + "/build"));
