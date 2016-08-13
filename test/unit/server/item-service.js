26ca8d107b72dae9d88da052a48aa49a57f36214
David Nimmo <dnimmo@gmail.com>
2016-08-14 00:30:24 +0100

    tidied up test calls

---
+++ test/unit/server/item-service.js
@@ -0,0 +1,6 @@
+// const test = require('tape')
+// const { readFileSync } = require('fs')
+// const items = JSON.parse(readFileSync('test/unit/resources/mock-items.json')).items
+// const { combineItems }
+//
+// test('')
--- test/unit/server/map-service.js
+++ test/unit/server/map-service.js
@@ -1,4 +1,4 @@
-const test = require('tape')
+const tape = require('tape')
 const { readFileSync } = require('fs')
 const map = JSON.parse(readFileSync('test/unit/resources/mock-map.json', 'utf8')).rooms
 const {
@@ -9,55 +9,53 @@ const {
   getItem
   } = require('../../../server/game-map/game-map-service')(map, 'test/unit/resources/test-logs', 'test-room.log')
 
-test('Map Service:', t => {
-  test('returns requested room', t => {
-    const testRoom = getRoom('start')
+const test = (description, func) => tape.call(null, `Map Service: ${description}`, func)
 
-    t.equal(testRoom.name, 'Foyer')
-    t.end()
-  })
+test('returns requested room', t => {
+  const testRoom = getRoom('start')
 
-  test('returns false if requested room does not exist', t => {
-    const testRoom = getRoom('this-room-does-not-exist')
+  t.equal(testRoom.name, 'Foyer')
+  t.end()
+})
 
-    t.equal(testRoom, false)
-    t.end()
-  })
+test('returns false if requested room does not exist', t => {
+  const testRoom = getRoom('this-room-does-not-exist')
 
-  test('can tell if an item can be used in a given room', t => {
-    const testRoomSlug = 'test-room'
-    const testItemUseInfo = ['test-room']
-    const result = canItemBeUsed(testItemUseInfo, testRoomSlug)
+  t.equal(testRoom, false)
+  t.end()
+})
 
-    t.equal(result, true)
-    t.end()
-  })
+test('can tell if an item can be used in a given room', t => {
+  const testRoomSlug = 'test-room'
+  const testItemUseInfo = ['test-room']
+  const result = canItemBeUsed(testItemUseInfo, testRoomSlug)
 
-  test('can return extra information about a given room', t => {
-    const testRoomSlug = 'test-room'
-    const testRoomExtraInfo = 'Some extra info you get when examining the room'
-    const { description } = examineRoom(testRoomSlug)
+  t.equal(result, true)
+  t.end()
+})
 
-    t.equal(description, testRoomExtraInfo)
-    t.end()
-  })
+test('can return extra information about a given room', t => {
+  const testRoomSlug = 'test-room'
+  const testRoomExtraInfo = 'Some extra info you get when examining the room'
+  const { description } = examineRoom(testRoomSlug)
 
-  test('can return an item when the room is examined, if one is present', t => {
-    const testRoomSlug = 'test-room'
-    const testItemName = 'test-item'
-    const { item } = examineRoom(testRoomSlug)
+  t.equal(description, testRoomExtraInfo)
+  t.end()
+})
 
-    t.equal(item, testItemName)
-    t.end()
-  })
+test('can return an item when the room is examined, if one is present', t => {
+  const testRoomSlug = 'test-room'
+  const testItemName = 'test-item'
+  const { item } = examineRoom(testRoomSlug)
 
-  test('returns false if an examined room has no item', t => {
-    const testRoomSlug = 'start'
-    const { item } = examineRoom(testRoomSlug)
+  t.equal(item, testItemName)
+  t.end()
+})
 
-    t.equal(item, false)
-    t.end()
-  })
+test('returns false if an examined room has no item', t => {
+  const testRoomSlug = 'start'
+  const { item } = examineRoom(testRoomSlug)
 
+  t.equal(item, false)
   t.end()
 })
